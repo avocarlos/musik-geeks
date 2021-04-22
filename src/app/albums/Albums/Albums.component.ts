f5c3781b5dd0ab6013e363a0153377d1e8260d31
jmonterovalverdeMISO <j.monterov@uniandes.edu.co>
2021-04-22 14:05:46 -0600

    Agregando estructura de página general

---
+++ src/app/albums/Albums/Albums.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Albums',
+  templateUrl: './Albums.component.html',
+  styleUrls: ['./Albums.component.css']
+})
+export class AlbumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/albums/Albums/Albums.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlbumsComponent } from './Albums.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AlbumsComponent]
+})
+export class AlbumsModule { }
