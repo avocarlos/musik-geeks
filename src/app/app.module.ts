6328644c98f4901cbac82e89ba924d1e9e517dcb
mgaitans <m.gaitans@uniandes.edu.co>
2021-04-24 13:25:45 -0500

    Se crear el módulo para consultar el listado de coleccionistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AlbumsModule} from './albums/albums.module'
+import { CollectorsModule } from './collectors/Collectors/Collectors.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {AlbumsModule} from './albums/albums.module'
     BrowserModule,
     AppRoutingModule,
     AlbumsModule,
-    HttpClientModule
+    HttpClientModule,
+    CollectorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/collectors/Collectors/Collectors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collectors',
+  templateUrl: './Collectors.component.html',
+  styleUrls: ['./Collectors.component.css']
+})
+export class CollectorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
