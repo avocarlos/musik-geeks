b8ca4cec49482917871928fc5f572e01bae906d5
mgaitans <m.gaitans@uniandes.edu.co>
2021-04-25 19:13:30 -0500

    Ajustes HU05: se cambia el nombre a baseUrl para ajustar el casing, se cambia el modulo para una sola carpeta, se cambia el diseño de la tablas, usando las construidas con bootstrap y se crear los test del servios y componente

--- src/app/collectors/collectors.component.ts
+++ src/app/collectors/collectors.component.ts
@@ -4,21 +4,21 @@ import { CollectorsService } from './collectors.service';
 
 @Component({
   selector: 'app-collectors',
-  templateUrl: './Collectors.component.html',
-  styleUrls: ['./Collectors.component.css']
+  templateUrl: './collectors.component.html',
+  styleUrls: ['./collectors.component.css']
 })
 export class CollectorsComponent implements OnInit {
 
-  constructor(private collectorsService: CollectorsService) { }
+  constructor(private collectorService: CollectorsService) { }
   public collectors: Array<Collector> = [];
 
   getCollectorsList(): void {
-    this.collectorsService.getCollectors().subscribe(cs => {
+    this.collectorService.getCollectors().subscribe(cs => {
       this.collectors = cs;
     });
   }
+
   ngOnInit(): void {
     this.getCollectorsList();
   }
-
 }
