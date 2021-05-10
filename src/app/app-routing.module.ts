d317aac207d74c62c2670da4e19af4067e18b6e7
Maria Angelica Gaitan Sanchez <m.gaitans@uniandes.edu.co>
2021-05-09 21:01:32 -0500

    Se agrega route al consultar el listado de coleccionistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MusicianListComponent } from './musician/musician-list/musician-list.component';
+import { CollectorsComponent } from './collectors/collectors.component';
 
-const routes: Routes = [{
-  path: 'musicos',
-  component: MusicianListComponent
-}];
+const routes: Routes = [
+  { path: 'musicos', component: MusicianListComponent },
+  { path: 'collectors', component: CollectorsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
