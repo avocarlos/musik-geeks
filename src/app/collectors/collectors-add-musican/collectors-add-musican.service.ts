2da5499fb4eebc053163e7818321c95fb40d639f
Maria Angelica Gaitan Sanchez <m.gaitans@uniandes.edu.co>
2021-05-23 12:17:00 -0500

    Se adiciona metodo post para agregar musico al coleccionista

--- src/app/collectors/collectors-add-musican/collectors-add-musican.service.ts
+++ src/app/collectors/collectors-add-musican/collectors-add-musican.service.ts
@@ -3,16 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 import { Collector } from '../collector';
+import { Musician } from '../../musician/musician';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CollectorsAddMusicanService {
 
-  private apiUrl = environment.baseUrl + 'collectors';
+  //private apiUrl = environment.baseUrl + 'collectors';
 
   constructor(private http: HttpClient) { }
   addCollerMusican(idc: number, idm: number): Observable<Collector> {
-    return this.http.get<Collector>(`${this.apiUrl}/${idc}/musicians/${idm}`);
+    const apiUrl = environment.baseUrl + 'collectors/'+idc+'/musicians/' + idm;
+    const musiadd = environment.baseUrl + 'musicians/' + idm;
+    return this.http.post<Collector>(apiUrl,musiadd);
   }
 }
